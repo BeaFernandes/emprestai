45e77d05ce266e039f863aa9d605c2af8a8be2e2
BeaFernandes <beatrizalmeida354@gmail.com>
2019-11-13 20:12:38 -0300

    Concertado menu mobile

---
+++ assets/resources/scripts/common.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
