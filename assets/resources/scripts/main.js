4c9e88c09f441c3e86b75a19f3390fbad7b91332
BeaFernandes <beatrizalmeida354@gmail.com>
2019-11-14 15:48:10 -0300

    Iniciado formulário

--- assets/resources/scripts/main.js
+++ assets/resources/scripts/main.js
@@ -3,3 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {});
+  });
